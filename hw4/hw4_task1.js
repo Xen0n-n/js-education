fad598a00a291a212220f8ceb514e55ae9b843d2
Xen0n-n <nsoft13@gmail.com>
2024-03-20 21:29:26 +0300

    fixed task1 hw4

--- hw4/hw4_task1.js
+++ hw4/hw4_task1.js
@@ -1,5 +1,9 @@
 'use strict'
 function number_parser(number) {
+        if (number > 999) {
+        console.log("Введенное число больше 999!")
+        return {}
+    }
     let num_obj = {
         hundreds: Math.floor(number / 100),
         tens: Math.floor((number % 100) / 10),
