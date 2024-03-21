f0fa0f84e9d95641743a084acd6147bf41314d22
Xen0n-n <nsoft13@gmail.com>
2024-03-21 13:34:28 +0300

    updatet class implementation

--- lesson4/lesson4_classes.js
+++ lesson4/lesson4_classes.js
@@ -10,7 +10,28 @@ class Person {
     }
 }
 
+
+class Person_extended extends Person {
+    constructor(name, age, gender, location, hobby) {
+        super(name, age, gender);
+        this.location = location;
+        this.hobby = hobby;
+    }
+
+    SayName() {
+        console.log(`My name is ${this.name}`);
+    }
+}
+
+
 const person1 = new Person("Denis", 25, "male");
 console.log(person1.age);
-person1.incrementAge();
-console.log(person1.age);
+
+
+
+
+const person2 = new Person_extended("Andrey", 22, "male", "Moscow", "bananas")
+person2.SayName();
+
+
+
