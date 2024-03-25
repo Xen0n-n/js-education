07452f00ce88021af6e438a519f8dba00b92d09c
Xen0n-n <nsoft13@gmail.com>
2024-03-25 15:55:37 +0300

    codewars added

---
+++ codewars/rock-scissors-paper.js
@@ -0,0 +1,40 @@
+const rps = (p1, p2) => {
+    if (p1 === 'scissors') {
+        switch (p2) {
+            case 'paper':
+                return 'Player 1 won!';
+                break;
+            case 'rock':
+                return 'Player 2 won!';
+                break;
+            case 'scissors':
+                return 'Draw!';
+                break;
+        }
+    } else if (p1 === 'paper') {
+        switch (p2) {
+            case 'paper':
+                return 'Draw!';
+                break;
+            case 'rock':
+                return 'Player 1 won!';
+                break;
+            case 'scissors':
+                return 'Player 2 won!';
+                break;
+        }
+    }
+    else if (p1 === 'rock') {
+        switch (p2) {
+            case 'paper':
+                return 'Player 2 won!';
+                break;
+            case 'rock':
+                return 'Draw!';
+                break;
+            case 'scissors':
+                return 'Player 1 won!';
+                break;
+        }
+    }
+};
