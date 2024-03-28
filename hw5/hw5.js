90d877b283d6e62463efae0951388087893d71b7
Xen0n-n <nsoft13@gmail.com>
2024-03-28 09:42:38 +0300

    formatted chess_map

--- hw5/hw5.js
+++ hw5/hw5.js
@@ -1,12 +1,5 @@
 let main_block = document.getElementById("main-content");
 
-
-let white_block = document.createElement("td");
-white_block.style.width = "60px";
-white_block.style.height = "60px";
-white_block.style.backgroundColor = "yellow";
-
-
 function createChessMap() {
     let chess_map = document.createElement("table");
     
@@ -21,8 +14,6 @@ function createChessMap() {
         for (let j = 0; j < 8; j++) {
             
             let block = document.createElement("td");
-            block.style.width = "5em";
-            block.style.height = "5em";
             chess_map_row.appendChild(block);
             block.style.backgroundColor = counter % 2 === 0 ? "black" : "white";
             if (j !== 7){
