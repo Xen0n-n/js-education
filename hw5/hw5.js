daf26c214e49921964d0dbc41b92f7413c68d0e6
Xen0n-n <nsoft13@gmail.com>
2024-03-28 14:34:43 +0300

    krasivoe pole dlya chess

--- hw5/hw5.js
+++ hw5/hw5.js
@@ -1,23 +1,52 @@
 let main_block = document.getElementById("main-content");
 
 function createChessMap() {
+    let alphabet = "ABCDEFGH";
+
     let chess_map = document.createElement("table");
-    
+
     main_block.appendChild(chess_map);
-    
+
     let counter = 1;
-    for (let i = 0; i < 8; i++) {
-        
+
+    for (let i = 0; i <= 8; i++) {
+
         let chess_map_row = document.createElement("tr");
         chess_map.appendChild(chess_map_row);
-        
-        for (let j = 0; j < 8; j++) {
-            
+
+        if (i === 0) {
             let block = document.createElement("td");
             chess_map_row.appendChild(block);
-            block.style.backgroundColor = counter % 2 === 0 ? "black" : "white";
-            if (j !== 7){
-                counter++;
+        }
+        
+        for (let j = 0; j <= 8; j++) {
+
+            if (i === 0 && j !== 0) {
+                let td = document.createElement('td');
+                td.innerText = j;
+                td.style.width = "2em";
+                td.style.border = "solid";
+                td.style.textAlign = "center";
+                chess_map_row.appendChild(td);
+            }
+            
+            if (j === 0 && i !== 0) {
+                let td = document.createElement('td');
+                td.innerText = alphabet[i - 1];
+                td.style.width = "2em";
+                td.style.border = "solid";
+                td.style.textAlign = "center";
+                chess_map_row.appendChild(td);
+
+            }
+            
+            if (i !== 0 && j !== 0) {
+                let block = document.createElement("td");
+                chess_map_row.appendChild(block);
+                block.style.backgroundColor = counter % 2 === 0 ? "black" : "white";
+                if (j !== 8) {
+                    counter++;
+                }
             }
         }
     }
